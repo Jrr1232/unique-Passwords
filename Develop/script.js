b42a03ad4029097944eb35d0789b5baaedd2dca0
Joaquin Florencio <Jrr232@gmail.com>
2023-05-06 18:09:39 -0400

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -9,25 +9,18 @@ function writePassword() {
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var password = [];
-
-
+  var specialCharacter;
+  var passwordLength;
+  var lowerCaseCharacters;
+  var numbersAllowed;
   passwordSize();
   lowerCase();
   specialCharacters();
-
-  //make an array
-  //keep addings specific things until i reach the passwordlength desired
-  //concat the array
+  numbers();
+  alert("Password length chosen" + ":" + " " + passwordLength);
 
 
 
-  var specialCharacter;
-  var passwordLength;
-
-  alert(passwordLength);
-
-
-  /// if (specialCharacter === "true" || specialCharacter === "yes") {
 
 
   for (i = 0; i < passwordLength; i++) {
@@ -36,32 +29,58 @@ function writePassword() {
     var text = "!@#$&()\\-`.+,/\"]*$";
     var specialCharactersArray = text.split('');
     var randomSpecial = specialCharactersArray[randomNumber];
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     var randomLetter = alphabet[randomNumber];
     var randomThreeNumbers = Math.floor(Math.random() * 3);
-
     var randomSelectionArray = []
-    randomSelectionArray.push(randomNumber, randomLetter, randomSpecial)
     var character = randomSelectionArray[randomThreeNumbers];
-    console.log(randomSelectionArray);
-    console.log(character);
-    password.push(character);
 
 
 
+
+    if (numbersAllowed === false && lowerCaseCharacters === false && specialCharacter === false) {
+      var upperCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+
+      upperCaseAlphabet = upperCaseAlphabet.toUpperCase();
+      var randomUpperCaseLetter = upperCaseAlphabet[randomNumber]
+      randomSelectionArray.push(randomUpperCaseLetter, randomUpperCaseLetter, randomUpperCaseLetter);
+      var character = randomSelectionArray[randomThreeNumbers];
+      password.push(character);
+
+
+
+
+
+    } else if (lowerCaseCharacters === true && numbersAllowed === true && specialCharacter === true) {
+      randomSelectionArray.push(randomNumber, randomLetter, randomSpecial)
+      var character = randomSelectionArray[randomThreeNumbers];
+      console.log(randomSelectionArray);
+      console.log(character);
+      password.push(character);
+    } else if (lowerCaseCharacters === false && numbersAllowed === true && specialCharacter === true) {
+      var upperCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+
+      upperCaseAlphabet = upperCaseAlphabet.toUpperCase();
+      var randomUpperCaseLetter = upperCaseAlphabet[randomNumber]
+      randomSelectionArray.push(randomUpperCaseLetter, randomNumber, randomSpecial)
+      var character = randomSelectionArray[randomThreeNumbers];
+      password.push(character);
+
+
+    }
 
 
   }
-  // password = password.sort((a, b) => 0.5 - Math.random());
 
 
 
-  // } else {
-  //   for (i = 0; i < passwordLength; i++) {
-  //     password.push(randomNumber);
 
-  //   }
-  // }
+
+
+
+
 
 
 
@@ -81,13 +100,12 @@ function writePassword() {
 
     } else if (passwordLength < 8) {
       alert("Must be more than 8 characters")
-      passwordLength = prompt("Please enter character length between 8 and 128 characters");
+
 
 
 
     } else if (passwordLength > 128) {
       alert("Must be less than 128 characters")
-      passwordLength = prompt("Please enter character length between 8 and 128 characters");
 
 
     } else {
@@ -102,22 +120,39 @@ function writePassword() {
 
   }
 
+  function numbers() {
+
+    var responce = prompt("Would you like to include numbers?");
+    if (responce == "yes" || responce == "true") {
+      numbersAllowed = true;
+    } else if (responce == "false" || responce == "no") {
+      numbersAllowed = false;
+    } else {
+      responce = prompt("Please enter yes/true or false/no")
+      alert(responce);
+
+
+    }
+  }
+
+
+
 
   // Confirms character types to be included in password
   function lowerCase() {
-    var lowerCaseCharacters
 
 
 
-    var answer = prompt("Would you like to use lowerCase")
-    if (answer == "yes" || answer == "true") {
-      lowerCaseCharacters === "true";
+
+    var responce = prompt("Would you like to use lowerCase?")
+    if (responce == "yes" || responce == "true") {
+      lowerCaseCharacters = true;
 
 
-    } else if (answer == "no" || answer == "false") {
-      lowerCaseCharacters === "false";
+    } else if (responce == "no" || responce == "false") {
+      lowerCaseCharacters = false;
     } else {
-      alert("Please enter yes/true or no/false")
+      return;
 
     }
     return;
@@ -127,18 +162,18 @@ function writePassword() {
   function specialCharacters() {
 
 
-    var responce = prompt("Would you like to include special characters and numbers")
-    var specialCharacter;
+    var responce = prompt("Would you like to include special characters: '$/?#!@#$'?")
+
     if (responce == "yes" || responce == "true") {
-      specialCharacter === "true";
+      specialCharacter = true;
 
 
     } else if (responce == "no" || responce == "false") {
-      specialCharacter === "false";
+      specialCharacter = false;
 
     } else {
-      responce = prompt("Please enter yes/true or no/false")
 
+      return;
     }
     return;
 
